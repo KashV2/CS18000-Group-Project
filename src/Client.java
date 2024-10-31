077f54e2cd05cdee9d192039591059aba64c8007
Future Inspire Games <shunsei.chan@gmail.com>
2024-10-31 18:23:36 -0400

    Updating Client, Server, and ClientHandler

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,62 @@
 import java.net.*;
 import java.io.*;
+import java.util.Scanner;
 
 public class Client {
+    public static void main(String[] args) {
+        Socket server = createServer();
+        if (server == null) return;
+        System.out.println("Connected to Server!");
 
+        Scanner scanner = new Scanner(System.in);
+
+        //Validate input
+        int input = 0;
+        while (true) {
+            System.out.println("Do you want to sign in or create a new account?\n1 Sign in\n2 Create new account");
+            try {
+                input = Integer.parseInt(scanner.nextLine());
+                if (input != 1 && input != 2) {
+                    throw new RuntimeException();
+                }
+                break;
+            } catch (RuntimeException e) {
+                closeServer(server);
+                System.out.println("Invalid input!");
+            }
+        }
+
+        System.out.println("Please enter your username: ");
+        String loginUsername = scanner.nextLine();
+
+        System.out.println("Please enter your password: ");
+        String password = scanner.nextLine();
+        switch (input) {
+            case 1:
+                //Send a login request to the sever
+                break;
+            case 2:
+                //Send a create new account request to the server;
+                break;
+            default:
+        }
+    }
+    
+    private static Socket createServer() {
+        try {
+            Socket server = new Socket("10.24.176.212", 9000);
+            return server;
+        } catch (Exception e) {
+            System.out.println("Failed to connect to the server!");
+            return null;
+        }
+    }
+    
+    private static void closeServer(Socket server) {
+        try {
+            server.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/Server.java
+++ src/Server.java
@@ -3,4 +3,46 @@ import java.io.*;
 
 public class Server {
 
+
+    public static void main(String[] args) {
+        ServerSocket serverSocket = createServerSocket();
+        if (serverSocket == null) return;
+
+        while (true) {
+            Socket client = createClient(serverSocket);
+            if (client == null) {
+                closeServerSocket(serverSocket);
+                return;
+            }
+
+        }
+    }
+
+    private static ServerSocket createServerSocket() {
+        try {
+            ServerSocket ss = new ServerSocket(9000);
+            return ss;
+        } catch (IOException e) {
+            System.out.println("Could not create server socket");
+            return null;
+        }
+    }
+
+    private static void closeServerSocket(ServerSocket ss) {
+        try {
+            ss.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Socket createClient(ServerSocket serverSocket) {
+        try {
+            Socket client = serverSocket.accept();
+            return client;
+        } catch (IOException e) {
+            System.out.println("Could not create client");
+            return null;
+        }
+    }
 }
