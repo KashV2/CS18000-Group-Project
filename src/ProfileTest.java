e9de331d46e5ea02f2282e93f13122fe039234a6
Owner <giabachannd@gmail.com>
2024-11-17 15:27:07 -0500

    update Profile test

--- src/ProfileTest.java
+++ src/ProfileTest.java
@@ -1,8 +1,8 @@
 import org.junit.jupiter.api.Test;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import static org.junit.jupiter.api.Assertions.*;
+
 /**
  * The ProfileTest program. Responsible for validating the Profile class.
  *
@@ -13,66 +13,90 @@ import static org.junit.jupiter.api.Assertions.*;
  * @version November 3, 2024
  */
 class ProfileTest {
+
     @Test
     void setDescriptionTest() {
         Profile testProfile = new Profile("a", "generic description",
-            new ArrayList<String>(), new ArrayList<String>());
+                new ArrayList<>(), new ArrayList<>());
         testProfile.setDescription("test");
         assertEquals("test", testProfile.getDescription());
     }
+
     @Test
     void setNameTest() {
         Profile testProfile = new Profile("a", "generic description",
-            new ArrayList<String>(), new ArrayList<String>());
+                new ArrayList<>(), new ArrayList<>());
         testProfile.setName("test");
         assertEquals("test", testProfile.getName());
     }
+
     @Test
     void removeFriendTest() {
         Profile testProfile = new Profile("a", "generic description",
-            new ArrayList<String>(Arrays.asList("b", "c", "d")), new ArrayList<String>());
+                new ArrayList<>(Arrays.asList("b", "c", "d")), new ArrayList<>());
         testProfile.removeFriend("b");
-        ArrayList<String> expectedFriends = new ArrayList<String>(Arrays.asList("c", "d"));
+        ArrayList<String> expectedFriends = new ArrayList<>(Arrays.asList("c", "d"));
         assertEquals(expectedFriends, testProfile.getFriends());
     }
+
     @Test
     void addFriendTest() {
         Profile testProfile = new Profile("a", "generic description",
-            new ArrayList<String>(Arrays.asList("b", "c", "d")), new ArrayList<String>());
+                new ArrayList<>(Arrays.asList("b", "c", "d")), new ArrayList<>());
         testProfile.addFriend("e");
         ArrayList<String> expectedFriends = new ArrayList<>(Arrays.asList("b", "c", "d", "e"));
         assertEquals(expectedFriends, testProfile.getFriends());
     }
+
     @Test
     void removeBlockTest() {
         Profile testProfile = new Profile("a", "generic description",
-            new ArrayList<String>(), new ArrayList<String>(Arrays.asList("b", "c", "d")));
+                new ArrayList<>(), new ArrayList<>(Arrays.asList("b", "c", "d")));
         testProfile.removeBlock("b");
-        ArrayList<String> expectedBlock = new ArrayList<>(Arrays.asList("c", "d"));
-        assertEquals(expectedBlock, testProfile.getBlockedUsers());
+        ArrayList<String> expectedBlocked = new ArrayList<>(Arrays.asList("c", "d"));
+        assertEquals(expectedBlocked, testProfile.getBlockedUsers());
     }
+
     @Test
     void addBlockTest() {
         Profile testProfile = new Profile("a", "generic description",
-            new ArrayList<String>(), new ArrayList<String>(Arrays.asList("b", "c", "d")));
+                new ArrayList<>(), new ArrayList<>(Arrays.asList("b", "c", "d")));
         testProfile.addBlock("e");
-        ArrayList<String> expectedBlock = new ArrayList<>(Arrays.asList("b", "c", "d", "e"));
-        assertEquals(expectedBlock, testProfile.getBlockedUsers());
+        ArrayList<String> expectedBlocked = new ArrayList<>(Arrays.asList("b", "c", "d", "e"));
+        assertEquals(expectedBlocked, testProfile.getBlockedUsers());
     }
+
     @Test
-    void isFriendTest() {
-        Profile testProfile = new Profile("a", "generic description",
-            new ArrayList<String>(Arrays.asList("b", "c", "d")), new ArrayList<String>());
+    void isFriendTestWithProfile() {
+        Profile testProfile1 = new Profile("a", "generic description",
+                new ArrayList<>(Arrays.asList("b", "c", "d")), new ArrayList<>());
         Profile testProfile2 = new Profile("b", "generic description",
-            new ArrayList<String>(Arrays.asList("e", "f", "g")), new ArrayList<String>());
-        assertTrue(testProfile.isFriend(testProfile2));
+                new ArrayList<>(Arrays.asList("e", "f", "g")), new ArrayList<>());
+        assertTrue(testProfile1.isFriend(testProfile2));
     }
+
     @Test
-    void isBlockedTest() {
+    void isFriendTestWithUser() {
         Profile testProfile = new Profile("a", "generic description",
-            new ArrayList<String>(), new ArrayList<String>(Arrays.asList("b", "c", "d")));
+                new ArrayList<>(Arrays.asList("b", "c", "d")), new ArrayList<>());
+        User testUser = new User("b", "password", new Profile("b", "desc", new ArrayList<>(), new ArrayList<>()));
+        assertTrue(testProfile.isFriend(testUser));
+    }
+
+    @Test
+    void isBlockedTestWithProfile() {
+        Profile testProfile1 = new Profile("a", "generic description",
+                new ArrayList<>(), new ArrayList<>(Arrays.asList("b", "c", "d")));
         Profile testProfile2 = new Profile("b", "generic description",
-            new ArrayList<String>(), new ArrayList<String>(Arrays.asList("e", "f", "g")));
-        assertTrue(testProfile.isBlocked(testProfile2));
+                new ArrayList<>(), new ArrayList<>(Arrays.asList("e", "f", "g")));
+        assertTrue(testProfile1.isBlocked(testProfile2));
+    }
+
+    @Test
+    void isBlockedTestWithUser() {
+        Profile testProfile = new Profile("a", "generic description",
+                new ArrayList<>(), new ArrayList<>(Arrays.asList("b", "c", "d")));
+        User testUser = new User("b", "password", new Profile("b", "desc", new ArrayList<>(), new ArrayList<>()));
+        assertTrue(testProfile.isBlocked(testUser));
     }
 }
