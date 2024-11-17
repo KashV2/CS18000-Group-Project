6f0ee6a1d2580cb7385d5fdc34b53ebfef134693
Future Inspire Games <shunsei.chan@gmail.com>
2024-11-16 20:45:49 -0500

    jkjfa

--- src/RunLocalTest.java
+++ src/RunLocalTest.java
@@ -1,12 +1,12 @@
 public class RunLocalTest {
     public static void main(String[] args) {
-        org.junit.platform.console.ConsoleLauncher.main(new String[]{            
-        "--select-class", "ChatDatabaseTest",
-        "--select-class", "ChatTest",
-        "--select-class", "ClientTest",
-        "--select-class", "DatabaseTest",
-        "--select-class", "ProfileTest",
-        "--select-class", "UserTest"});
+//        org.junit.platform.console.ConsoleLauncher.main(new String[]{
+//        "--select-class", "ChatDatabaseTest",
+//        "--select-class", "ChatTest",
+//        "--select-class", "ClientTest",
+//        "--select-class", "DatabaseTest",
+//        "--select-class", "ProfileTest",
+//        "--select-class", "UserTest"});
     }
 }
 
